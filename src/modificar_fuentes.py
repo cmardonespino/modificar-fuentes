caf2210ea3af601df87da78cc6b692d569c4cdd0
sentra-excmapi <cmardonespino@gmail.com>
2018-06-20 18:56:40 -0400

    Se hacen modificaciones

--- src/modificar_fuentes.py
+++ src/modificar_fuentes.py
@@ -141,6 +141,8 @@ for i in range(len(lines)):
 
 nuevo_archivo.writelines(nuevo_contenido_archivo)
 '''
+'''
+#VERSION 3
 os.system('mkdir '+path+'\\output')
 def method(lines, archivo):
 	#VERSION 3
@@ -163,6 +165,49 @@ def method(lines, archivo):
 				nuevo_contenido_archivo.append('#'+lines[i])
 		elif '#produccion' in lines[i].lower() or \
 		'#filesystem produccion' in lines[i].lower():
+			a = 1
+			nuevo_contenido_archivo.append(lines[i])
+		else:
+			if a == 2 and lines[i] != '\n':
+				if '#' in lines[i]:
+					nuevo_contenido_archivo.append(lines[i])
+				else:
+					nuevo_contenido_archivo.append('#'+lines[i])
+			elif a == 1 and lines[i] != '\n':
+				if '#' in lines[i]:
+					newstr = lines[i].replace("#", "")
+					nuevo_contenido_archivo.append(newstr)
+				else:
+					nuevo_contenido_archivo.append(lines[i])
+			else:
+				a=0
+				nuevo_contenido_archivo.append(lines[i])
+
+	nuevo_archivo.writelines(nuevo_contenido_archivo)
+'''
+
+os.system('mkdir '+path+'\\output')
+def method(lines, archivo):
+	#VERSION 3
+	i=0 
+	a=0 
+	nuevo_archivo = open(path+'\\output\\'+archivo+'.txt', 'w') 
+	nuevo_contenido_archivo = []
+
+	for i in range(len(lines)):
+		if 'desarrollo' in lines[i].lower() or \
+		'integracion' in lines[i].lower() or \
+		'certificacion' in lines[i].lower() or \
+		'filesystem desarrollo' in lines[i].lower() or \
+		'filesystem integracion' in lines[i].lower() or \
+		'filesystem certificacion' in lines[i].lower():
+			a = 2
+			if '#' in lines[i]:
+				nuevo_contenido_archivo.append(lines[i])
+			else:
+				nuevo_contenido_archivo.append('#'+lines[i])
+		elif 'produccion' in lines[i].lower() or \
+		'filesystem produccion' in lines[i].lower():
 			a = 1
 			nuevo_contenido_archivo.append(lines[i])
 		else:
