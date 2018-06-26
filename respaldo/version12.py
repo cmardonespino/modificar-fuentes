e1deafd7247d15e012b115dae6cdbb0867ac6981
sentra-excmapi <cmardonespino@gmail.com>
2018-06-26 11:53:50 -0400

    Se hacen modificaciones para para reducir lineas  de codigo y ademas para arreglar detalles

--- respaldo/version12.py
+++ respaldo/version12.py
@@ -8,26 +8,29 @@ path_tablas = path+'\\resources\\tablas'
 
 carpetas_en_el_directorio = os.listdir(path_tablas)
 
-#VERSION 14
+#VERSION 12
 os.system('mkdir '+path+'\\output')
-def method(lines, ambientes, ambiente):
+def method(lines, archivo, ambientes, ambiente):
 	ambientes_elem = set(ambientes)
 	i=0 
 	a=0 
+	nuevo_archivo = open(path+'\\output\\'+archivo+'.txt', 'w') 
 	nuevo_contenido_archivo = []
 
 	for i in range(len(lines)):
 		# se agrega unidecode por archivo AdministracionCampanas.parametros
 		if any(word in unidecode.unidecode(lines[i].lower()) for word in ambientes):
 			a = 2
-			if '#' in lines[i] or ';' in lines[i]:
+			if '#' in lines[i]:
+				nuevo_contenido_archivo.append(lines[i])
+			elif ';' in lines[i]:
 				nuevo_contenido_archivo.append(lines[i])
 			else:
 				nuevo_contenido_archivo.append('#'+lines[i])
 		elif ambiente in unidecode.unidecode(lines[i].lower()) or \
 		'filesystem '+ambiente in unidecode.unidecode(lines[i].lower()):
 			# condición agregada por atmpagodeservicios.parametros
-			if ambiente+':' in lines[i].lower():
+			if ambiente+':' in lines[i]:
 				newstr = lines[i].replace("#", "")
 				nuevo_contenido_archivo.append(newstr)
 			else:
@@ -47,33 +50,50 @@ def method(lines, ambientes, ambiente):
 						nuevo_contenido_archivo.append(newstr)
 					#agregado por archivo Riesgo.parametros
 					# linea 135
-					elif ';' in lines[i].lower():
+					elif 'alsgpyme' in lines[i].lower() and \
+					 ';' in lines[i].lower():
 						newstr = lines[i].replace("#", "")
 						nuevo_contenido_archivo.append(newstr)
 					else:
 						nuevo_contenido_archivo.append(lines[i])
 				elif '---' in lines[i]:
-					if '#' in lines[i] or ';' in lines[i] or '.\n' in lines[i]:
+					if '#' in lines[i]:
+						nuevo_contenido_archivo.append(lines[i])
+					elif ';' in lines[i]:
+						nuevo_contenido_archivo.append(lines[i])
+					elif '.\n' in lines[i]:
 						nuevo_contenido_archivo.append(lines[i])
 					else:
 						nuevo_contenido_archivo.append('#'+lines[i])
 				# condición agregada por atmpagodeservicios.parametros
 				elif 'WSDL_SERVIPAG;' in lines[i]:
 					nuevo_contenido_archivo.append(lines[i])
-				elif ';' in lines[i] or '==' in lines[i]:
+				elif ';' in lines[i]:
+					nuevo_contenido_archivo.append('#'+lines[i])
+				elif '==' in lines[i]:
 					nuevo_contenido_archivo.append('#'+lines[i])
 				else:
 					nuevo_contenido_archivo.append('#'+lines[i])
 			elif a == 1 and lines[i] != '\n':
 				if '#' in lines[i]:
-					if '---' in lines[i] or '.\n' in lines[i] or ' - ' in lines[i] or \
-					'==' in lines[i] or '#\n' in lines[i]:
+					if '---' in lines[i]:
+						nuevo_contenido_archivo.append(lines[i])
+					elif '.\n' in lines[i]:
+						nuevo_contenido_archivo.append(lines[i])
+					elif ' - ' in lines[i]:
+						nuevo_contenido_archivo.append(lines[i])
+					elif '==' in lines[i]:
+						nuevo_contenido_archivo.append(lines[i])
+					# se agrega por archivo alcancia.parametros
+					elif '#\n' in lines[i]:
 						nuevo_contenido_archivo.append(lines[i])
 					else:
 						newstr = lines[i].replace("#", "")
 						nuevo_contenido_archivo.append(newstr)
 				elif '---' in lines[i]:
-					if '#' in lines[i] or '.\n' in lines[i]:
+					if '#' in lines[i]:
+						nuevo_contenido_archivo.append(lines[i])
+					elif '.\n' in lines[i]:
 						nuevo_contenido_archivo.append(lines[i])
 					else:
 						nuevo_contenido_archivo.append('#'+lines[i])
@@ -87,7 +107,7 @@ def method(lines, ambientes, ambiente):
 				a=0
 				nuevo_contenido_archivo.append(lines[i])
 
-	return nuevo_contenido_archivo
+	nuevo_archivo.writelines(nuevo_contenido_archivo)
 
 ambientes_elem = set(ambientes)
 if ambiente in ambientes_elem:
@@ -97,5 +117,4 @@ if ambiente in ambientes_elem:
 for archivo in archivos_con_ambiente:
 	with open(path_tablas+'\\'+archivo,errors='ignore') as f:
 		lines = f.readlines()
-		nuevo_archivo = open(path+'\\output\\'+archivo+'.txt', 'w') 
-		nuevo_archivo.writelines(method(lines, ambientes, ambiente))
+		method(lines, archivo, ambientes, ambiente)
